96df9ebb127a58d9a52c158a20f6010b601abd6c
Matthew Day <matt@matthewday.net>
2021-04-10 19:25:27 -0400

    update solution with alt text

--- js/api/007/solution/main.js
+++ js/api/007/solution/main.js
@@ -3,18 +3,18 @@ fetch("https://api.unsplash.com/photos/random/?client_id=rSko55lJSBL2xZ_H_dClgHc
   .then((images) => {
     images.map((image, index) => {
       const isHorizontal = image.width/image.height >= 1;
-      replaceImage(`image${index}`, image.urls.small, isHorizontal)
+      const altText = image.alt_description;
+      replaceImage(`image${index}`, image.urls.small, isHorizontal, altText)
     })
   })
   .catch(err => { throw err });
 
 
 
-const replaceImage = (imageId, imagePath, isHorizontal) => {
+const replaceImage = (imageId, imagePath, isHorizontal, altText) => {
   const target = document.getElementById(imageId);
   const vertClass = !isHorizontal ? target.classList.add("vert") : "";
-  console.log(vertClass);
   target.innerHTML = `
-    <img src="${imagePath}" alt="wedding"/>
+    <img src="${imagePath}" alt="${altText}"/>
   `;
 };
